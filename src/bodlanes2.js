b450c675001b169cf0f445d6aadc6be0a9f7ca68
Dan Q <dan@scatmania.org>
2018-01-06 10:43:52 +0000

    Navigation and status

--- src/bodlanes2.js
+++ src/bodlanes2.js
@@ -7,18 +7,41 @@ import {tag, template} from 'slim-js/Decorators'
 function requireAll(r) { r.keys().forEach(r); }
 requireAll(require.context('./components/', true, /\.js$/));
 
-// Save/load current container content status (to the hash)
+// Save/load/set current container content status (to the hash)
 function saveStatus(){
-
+  const containers = [...document.querySelectorAll('bl-template .renderable')];
+  const status = containers.map((el)=>`${el.id}=${el.attributes.showing.value}`).join('&');
+  window.location.hash = status;
 }
 function loadStatus(){
-
+  const status = window.location.hash.replace(/^#/, '').split('&').map((i)=>i.split('=',2));
+  status.forEach((statusLine)=>{
+    //debugger;
+    document.querySelector(`bl-template bl-container#${statusLine[0]}`).attributes.showing.value = statusLine[1];
+  });
 }
-// Load initial status
-loadStatus();
+function navigate(id, target){
+  const container = document.querySelector(`bl-template bl-container#${target}`);
+  if(!container) return;
+  container.loadContent(id);
+  saveStatus();
+}
+
+// Link handling
+function documentClickHandler(e){
+  const el = e.target;
+  if((el.tagName == 'A') && el.attributes.href && el.attributes.target){
+    e.preventDefault();
+    navigate(el.attributes.href.value, el.attributes.target.value);
+  }
+}
+document.addEventListener('touchend', documentClickHandler);
+document.addEventListener('click', documentClickHandler);
 
 // Mark page as loaded
 window.addEventListener('load', (e)=>{
+  loadStatus();
   document.body.classList.add('loaded');
   document.querySelectorAll('bl-template .renderable').forEach((e)=>e.render());
+  saveStatus();
 });
